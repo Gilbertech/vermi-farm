e93c056235f7fa2897d81f1ca0c10b64989dba3a
gilbert <122779470+Gilbertech@users.noreply.github.com>
2025-06-27 10:19:51 +0300

    Updated Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -66,7 +66,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activeView, setActiveView, isOpen, se
 
 
               />
-            <<h1 className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold" style={{ color: '#983F21' }}>
+            <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold" style={{ color: '#983F21' }}>
   Vermi-Farm yetu
 </h1>
 
