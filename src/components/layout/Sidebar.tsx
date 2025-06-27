3df70f81a46a3816d298579aeb106f0c8be8c8b1
gilbert <122779470+Gilbertech@users.noreply.github.com>
2025-06-27 14:59:16 +0300

    Updated Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -65,7 +65,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activeView, setActiveView, isOpen, se
     alt="Vermi-Farm Logo"
     className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 xl:w-20 xl:h-20 rounded-full object-cover"
   />
-</div>
+
 
             </div>
             <button
