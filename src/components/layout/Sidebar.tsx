c6dd3c0f46e5ecb87c69a537af3f0229a23f79f2
gilbert <122779470+Gilbertech@users.noreply.github.com>
2025-06-27 15:00:43 +0300

    Updated Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -60,11 +60,14 @@ const Sidebar: React.FC<SidebarProps> = ({ activeView, setActiveView, isOpen, se
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-3">
               <div className="flex justify-center items-center py-4">
+<div className="w-full flex justify-center items-center pt-4">
   <img
     src="https://i.postimg.cc/MTpyCg68/logo.png"
     alt="Vermi-Farm Logo"
     className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 xl:w-20 xl:h-20 rounded-full object-cover"
   />
+</div>
+
               </div>
 
             </div>
