965da1360cdd58abcf0d16656fc74d6fda38b0fb
gilbert <122779470+Gilbertech@users.noreply.github.com>
2025-08-11 13:31:51 +0300

    Updated Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -33,7 +33,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activeView, setActiveView, isOpen, se
     { id: 'payments', icon: Wallet, label: 'Payments' },
     { id: 'statements', icon: FileText, label: 'Statements' },
     { id: 'portfolio', icon: Briefcase, label: 'Portfolio' },
-     { id: 'Msaidizi-Insurance', icon: Briefcase, label: 'Portfolio1' },
+     { id: 'Msaidizi-Insurance', icon: Briefcase, label: 'Msaidizi Insurance' },
   ];
 
   const handleMenuClick = (viewId: string) => {
